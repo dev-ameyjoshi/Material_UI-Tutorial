92ea0e504de18c3f6f2bc5c7dbbf49e4f1c2c510
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-02-26 16:38:55 +0530

    icon component

--- material-ui-react-tut/src/App.js
+++ material-ui-react-tut/src/App.js
@@ -1,23 +1,75 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+function CheckboxExample(){
+  const  [checked,setChecked] = React.useState(true) 
+  return(  
+    <FormControlLabel 
+      control={
+      <Checkbox 
+      checked={checked}
+      icon={<SaveIcon />} // if -> <DeleteIcon /> --> then save changes to that logo.
+      checkedIcon={<SaveIcon />}
+      onChange={(e) => setChecked(e.target.checked)}
+      color = "primary"
+      inputProps={{
+      'aria-label': 'secondary checkbox'
+      }}
+
+    />}
+    label="Testing Checkbox"
+    />
+    
+    
+    
+  )
+}
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Button 
-      onClick ={() => alert("Hello!")} 
-      // disabled --> disables the button.
-      size ="large" 
-      variant="contained" 
-      color="secondary">
-      Click Here !
-      </Button>
+      <CheckboxExample />
+      <ButtonGroup variant="contained" color="primary">
+        <Button 
+        startIcon={<SaveIcon />} //to apply icon at he start and similarly at the end.
+        // endIcon={<SaveIcon />}
+        onClick ={() => alert("Saved!")} 
+        // disabled --> disables the button.
+        style ={{
+          fontSize : 15
+          
+        }}
+        size ="large" 
+        
+        >
+              Save
+        </Button>
+        
+        <Button 
+        startIcon={<DeleteIcon />} //to apply icon at he start and similarly at the end.
+        // endIcon={<SaveIcon />}
+        onClick ={() => alert("Deleted!")} 
+        // disabled --> disables the button.
+        style ={{
+          fontSize : 15
+        }}
+        size ="large" 
+         
+        >
+            Discard
+        </Button>
+      </ButtonGroup>
         <img src={logo} className="App-logo" alt="logo" />
-        
-        
       </header>
     </div>
   );
