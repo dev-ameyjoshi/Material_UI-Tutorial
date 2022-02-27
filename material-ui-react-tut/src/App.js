8fcfc8c6af0219c3a27bb3c85b3222bc5b2626ed
Amey Joshi <ameyjoshi2001rj@gmail.com>
2022-02-27 12:02:41 +0530

    Tutorial complete

--- material-ui-react-tut/src/App.js
+++ material-ui-react-tut/src/App.js
@@ -7,15 +7,20 @@ import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import  TextField  from '@material-ui/core/TextField';
+// import  TextField  from '@material-ui/core/TextField';
 
 import {makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import { green,orange} from '@material-ui/core/colors';
 import 'fontsource-roboto';
 
 import  Typography  from '@material-ui/core/Typography';
-
-
+import  Container from '@material-ui/core/Container';
+import  Paper  from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import  AppBar from '@material-ui/core/AppBar';
+import  Toolbar from '@material-ui/core/Toolbar';
+import  IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles({
   root:{
@@ -35,15 +40,15 @@ const theme = createMuiTheme({
       marginBottom:15,
     }
   },
-  palette:{
-    primary:{
-      main:green[400],
+  // palette:{
+  //   primary:{
+  //     main:green[400],
 
-    },
-    secondary:{
-      main:orange[400],
-    }
-  }
+  //   },
+  //   secondary:{
+  //     main:orange[400],
+  //   }
+  // }
 })
 
 
@@ -83,22 +88,54 @@ function App() {
   
   return (
     <ThemeProvider theme = {theme}>
-    <div className="App">
+      <Container maxWidth="xs">
+      {/* xs --> md,lg  */}
+      <div className="App">
       <header className="App-header">
-      <Typography variant='h2'>
-      {/* component="div" is also another 
-      parameter if u want to use apart from 
-      regular component independently */}
-      Welcome to MUI
-      </Typography>
-      <Typography variant='subtitle1'>
-      {/* here variant can aslo be h1,h2... */}
-      Learn how to use material UI
-      </Typography>
+        <AppBar color="secondary">
+        <Toolbar>
+        <IconButton>
+        <MenuIcon />
+        </IconButton>
+        <Typography Variant="h6">
+        MUI Theme
+        </Typography>
+        <Button>
+        Login
+        </Button>
+        </Toolbar>
+        </AppBar>
+
+          <Typography variant='h2'>
+          {/* component="div" is also another 
+          parameter if u want to use apart from 
+          regular component independently */}
+          Welcome to MUI
+          </Typography>
+          <Typography variant='subtitle1'>
+          {/* here variant can aslo be h1,h2... */}
+          Learn how to use material UI
+          </Typography>
+
       <ButtonStyled /> 
 
+
+      <Grid container spacing={2} justify="center">
+          <Grid item xs={3} sm={6}> 
+              <Paper style={{height:75,width:'100%',}}/>
+          </Grid>
+          <Grid item xs={3} sm={6}>
+              <Paper style={{height:75,width:'100%',}}/>
+          </Grid>
+          <Grid item xs={3} sm={6}>
+              <Paper style={{height:75,width:'100%',}}/>
+              {/* Grid can be a container and an item */}
+          </Grid>
+      </Grid>
+
+
       {/* TextField component */}
-      <TextField 
+      {/* <TextField 
       variant='filled'
       color='secondary'
       type = 'email'
@@ -108,7 +145,7 @@ function App() {
       // above placeholder can also be value 
       //type can be changed to date,time,email
 
-      />
+      /> */}
 
 
       {/* Checkbox Component */}
@@ -141,11 +178,13 @@ function App() {
               Discard
         </Button>
       </ButtonGroup>
-        <img src={logo} className="App-logo" alt="logo" />
+        
+      <img src={logo} className="App-logo" alt="logo" />
        
       </header>
       
     </div>
+    </Container>
     </ThemeProvider>
   );
  
